86ade0fb7b88ecf11582566aeafa60d8a139cc3f
junghaeseok <haeru9410@gmail.com>
2023-08-28 22:21:43 +0900

    [ADD] 기본 버튼 ui 추가

--- app/src/common/components/button/ButtonCommon.tsx
+++ app/src/common/components/button/ButtonCommon.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { ButtonHTMLAttributes, Ref, forwardRef, useMemo } from 'react'
 import styles from '@/common/components/button/ButtonCommon.module.scss'
 
-const ButtonCommon = () => {
+type size = 'S' | 'M' | 'L'
+
+interface ButtonCommonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: size
+  children?: React.ReactNode
+}
+
+/**
+ * 기본 버튼
+ */
+const ButtonCommon = forwardRef(function ButtonCommon
+  (props: ButtonCommonProps, forwardRef: Ref<HTMLButtonElement>){
+
+  const {size = 'M', children, ...rest} = props
+
+  const buttonStyle = useMemo(() => {
+    return (size: size) => {
+      switch (size) {
+        case 'S':
+          return styles.s
+        case 'M':
+          return styles.m
+        case 'L':
+          return styles.l
+      }
+    }
+  }, [])
+
+
   return (
-    <button className={styles.button}>
-      버튼
+    <button
+      ref={forwardRef}
+      className={`${styles.button} ${buttonStyle(size)}`}
+      {...rest}
+    >
+      {children}
     </button>
   )
-}
+})
 
 export default ButtonCommon
